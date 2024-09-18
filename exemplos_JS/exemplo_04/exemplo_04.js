cd05f1874e528a50d7eace1934cc951d30619960
Douglas Lemos <dlfrontend09@gmail.com>
2024-09-17 23:29:21 -0300

    Atividades sobre JavaScript

---
+++ exemplos_JS/exemplo_04/exemplo_04.js
@@ -0,0 +1,55 @@
+
+
+// Declarando a variável
+
+let n = 10;
+
+// somando mais 5
+
+n += 5;
+
+console.log(n);
+
+// multiplicando por 5
+
+n *= 5;
+
+console.log(n)
+
+// somando mais 1
+
+++n;
+
+x = n%6 ** 2;
+alert(x)
+
+console.log(n)
+
+n/=4
+
+alert(n)
+
+
+// resto da divisão 
+
+
+
+// console.log(n);
+
+// (n % 6) ** 2;
+
+// alert(n)
+
+// alert(`O resto da divisão 'n' é ${n}`);
+
+// // elevando o resto da divisão ao quadrado
+
+// n = n**2;
+
+// alert(`O resto da divisão por 6, elevado ao quadrado é ${n}`);
+
+// // dividindo o valor por 4
+
+// n /=4;
+
+// alert(`O valor final da variável é ${n}`);
---
+++ exemplos_JS/exemplo_04/tempCodeRunnerFile.js
@@ -0,0 +1,21 @@
+n %= 6;
+
+// console.log(n);
+
+// (n % 6) ** 2;
+
+// alert(n)
+
+// alert(`O resto da divisão 'n' é ${n}`);
+
+// // elevando o resto da divisão ao quadrado
+
+// n = n**2;
+
+// alert(`O resto da divisão por 6, elevado ao quadrado é ${n}`);
+
+// // dividindo o valor por 4
+
+// n /=4;
+
+// alert(`O valor final da variável é ${n}`);
