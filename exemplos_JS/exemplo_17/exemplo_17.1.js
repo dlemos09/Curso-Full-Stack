0ec03b799cca87bb255a92cc01be3338e2667a3d
Douglas Lemos <dlfrontend09@gmail.com>
2024-09-19 15:34:14 -0300

    aula de for

---
+++ exemplos_JS/exemplo_17/exemplo_17.1.js
@@ -0,0 +1,31 @@
+
+//i significa index
+for (let i = 0; i <=5; i++) {
+    console.log(i);
+}
+
+console.clear();
+
+for (let i = 400; i <=500; i += 10) {
+    console.log(i);
+}
+console.clear();
+
+for (let i = -100; i <=500; i += 10) {
+    console.log(i);
+}
+
+console.clear();
+
+
+for (let i = 500; i >= 400; i -= 10) {
+    console.log(i);
+}
+
+console.clear();
+
+
+for (let i = 0; i <= 10; i++)    {
+    const par = i % 2 === 0;
+    console.log( i , par)
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
