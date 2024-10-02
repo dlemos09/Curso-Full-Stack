f6a9033b707c29d45bf7e709e04f47a00cf03827
Douglas Lemos <dlfrontend09@gmail.com>
2024-10-02 19:22:39 -0300

    adicionei material de funcao

---
+++ exemplos_JS/funcoes/exercicios_funcoes/area_triagulo/area_triangulo.js
@@ -0,0 +1,12 @@
+const base = document.getElementById('base')
+const altura = document.getElementById('altura')
+const output = document.getElementById('output')
+
+function mostrarArea() {
+    let baseValue = base.value
+    let alturaValue = altura.value
+
+    let area = (baseValue * alturaValue) / 2
+
+    output.innerHTML = `A área do triângulo de base ${baseValue} e altura ${alturaValue} é ${area}cm.`
+}
