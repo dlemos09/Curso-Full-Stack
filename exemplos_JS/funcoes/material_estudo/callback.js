8cbd86c66a43f617037c791ddc24f238c734936e
Douglas Lemos <dlfrontend09@gmail.com>
2024-10-04 18:47:20 -0300

    adicionei exercícios de funções e atualizei o material de estudo

--- exemplos_JS/funcoes/material_estudo/callback.js
+++ exemplos_JS/funcoes/material_estudo/callback.js
@@ -1,14 +1,13 @@
-function fn(cb){
-    console.log('Executar acao de callback')
-    console.log(typeof cb)
-    cb()
-}
+// Callback é um conceito importante em JavaScript que envolve o uso de funções como argumentos de outras funções. Uma função callback é simplesmente uma função que é passada como argumento para outra função e é chamada de volta em algum momento dentro da função externa. O uso mais comum de callbacks é para lidar com operações assíncronas, como leituras de arquivos ou requisições a servidores.
 
-fn(function(){
-    console.log('funcao passada por parametro')
-})
-
-function callback(){
-    console.log('funcao passada por paramentro')
-}
-fn(callback)
+function cumprimentar(nome, callback) {
+    console.log("Olá, " + nome);
+    callback();
+  }
+  
+  function dizerTchau() {
+    console.log("Até logo!");
+  }
+  
+  cumprimentar("João", dizerTchau);
+  
