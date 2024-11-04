2740b2d805980261d830061df01c28918249ae70
Douglas Lemos <dlfrontend09@gmail.com>
2024-11-04 20:25:50 -0300

    atualizei os arquivos

--- moduloNode/2_fundamentos/2_export_import/meu_modulo.mjs
+++ moduloNode/2_fundamentos/2_export_import/meu_modulo.mjs
@@ -1,5 +1,9 @@
-function soma (a,b){
-    console.log(a+b)
+// Define a função 'soma' que recebe dois parâmetros: 'a' e 'b'.
+function soma(a, b) {
+    // Exibe no console o resultado da soma de 'a' e 'b'.
+    console.log(a + b)
 }
 
+// Exporta a função 'soma' como a exportação padrão do módulo.
+// Isso permite que ela seja importada por outros módulos usando 'import soma from ...'.
 export default soma;
