29863888477578d525aca62b6c5d07fe7ae07b78
Douglas Lemos <dlfrontend09@gmail.com>
2025-12-10 19:16:44 -0300

    ajustes

--- modulo_JS/exercicios/exercicios_extras/exercicios_3.1_for/ex_03.js
+++ modulo_JS/exercicios/exercicios_extras/exercicios_3.1_for/ex_03.js
@@ -1,1 +1,8 @@
 // Enunciado: Usando for e if, imprima apenas os números pares entre 1 e 20.
+
+// Solução comentada
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 === 0) { // verifica se o resto da divisão por 2 é zero
+    console.log(i);
+  }
+}
